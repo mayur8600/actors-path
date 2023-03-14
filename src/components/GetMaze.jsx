ca79416ceca43b028db54ea269c646f39e2c7ece
mayur8600 <mayur.kamthe62987@gmail.com>
2023-03-14 17:20:08 +0530

    code cleanup

--- src/components/GetMaze.jsx
+++ src/components/GetMaze.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
+import React from "react";
 
-function GetMaze({maze}) {
+function GetMaze({ maze }) {
   return (
     <>
-    {
-        <div className='w-full flex direction-col justify-center overflow-scroll hideScroll'>
-        <div className='w-auto border'>{
-        maze?.map((singleMaze,i)=>{
-            return( <div key={i+'A'} className='flex direction-row'>{singleMaze.map((singleMazeElement,j)=>{
-                return (
-                    <div key={i+'B'+j}>
-                        {singleMazeElement === 'X' ? <div className='flex bg-black w-10 h-10 border-2 border-black items-center justify-center'>X</div> : singleMazeElement === 'S' ? <div className='bg-red-600 flex w-10 h-10 border-2 border-black items-center justify-center font-black'>S</div>: singleMazeElement === 'G'? <div className='bg-lime-600 flex w-10 h-10 border-2 border-black items-center justify-center font-black'>G</div>: <div className='bg-white w-10 h-10 border-2 border-black'></div>}
-                    </div>
-                )
-            })}</div>)
-        })
-}</div>
-</div>
-    }</>
-  )
+      {
+        <div className="w-full flex direction-col justify-center overflow-scroll hideScroll">
+          <div className="w-auto border">
+            {/* Mapping through Maze to make the UI for maze x-direction */}
+            {maze?.map((singleMaze, i) => {
+              return (
+                <div key={i + "A"} className="flex direction-row">
+                    {/* Mapping for y-direction */}
+                  {singleMaze.map((singleMazeElement, j) => {
+                    return (
+                      <div key={i + "B" + j}>
+                        {singleMazeElement === "X" ? (
+                          <div className="flex bg-black w-10 h-10 border-2 border-black items-center justify-center">
+                            X
+                          </div>
+                        ) : singleMazeElement === "S" ? (
+                          <div className="bg-red-600 flex w-10 h-10 border-2 border-black items-center justify-center font-black">
+                            S
+                          </div>
+                        ) : singleMazeElement === "G" ? (
+                          <div className="bg-lime-600 flex w-10 h-10 border-2 border-black items-center justify-center font-black">
+                            G
+                          </div>
+                        ) : (
+                          <div className="bg-white w-10 h-10 border-2 border-black"></div>
+                        )}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      }
+    </>
+  );
 }
 
-export default GetMaze
+export default GetMaze;
--- src/components/TextFileRead.jsx
+++ src/components/TextFileRead.jsx
@@ -8,20 +8,19 @@ function TextFileRead() {
   const [positionS, setPositionS] = useState([]);
   const [positionG, setPositionG] = useState([]);
   const [createdSolution, setCreatedSolution] = useState("");
-  const [solution, setSolution] = useState(false)
+  const [solution, setSolution] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (file) {
       let newMaze = [];
       const fileData = file?.target?.files[0];
+      // Compare Extension
       let extension = fileData.name.match(/(?<=\.)\w+$/g)[0].toLowerCase();
-      if (extension !== 'txt') {
-        alert('File Format Mismatch!');
+      if (extension !== "txt") {
+        alert("File Format Mismatch!");
         resetFile();
-        setFile()
-      }
-      else{
-
+        setFile();
+      } else {
         var fr = new FileReader();
         fr.readAsText(fileData);
         fr.onload = function () {
@@ -38,44 +37,82 @@ function TextFileRead() {
             newMaze.push(dataArr);
           }
           setMaze(newMaze);
+        };
       }
-      };
     }
-  },[file])
-
+  }, [file]);
+//  reset the input value
   function resetFile() {
-    const file = document.querySelector('.file');
-    file.value = '';
-    setSolution(false)
+    const file = document.querySelector(".file");
+    file.value = "";
+    setSolution(false);
   }
   return (
     <div className="max-w-5xl m-auto relative">
-    <div className="w-full text-center absolute cursive text-[40px]">Actor's Path</div>
+      <div className="w-full text-center absolute cursive text-[40px]">
+        Actor's Path
+      </div>
 
-        <div className={`w-full flex ${file && positionG.length && positionS.length ? 'pt-[80px] pb-2 justify-start' :'h-screen justify-center'}  items-center pb-10 z-0`}>
-        <input className="file" onChange={(e) => {setFile(e)}} type="file" />
-        {file && (!positionS.length || !positionG.length) &&<p className="mx-[10px] font-medium text-white">Please Enter valid maze</p>}
-        {file && <button
-        className="px-3 py-1 bg-[#FDD36A] rounded-[3px] hover:bg-lime-300  cursor-pointer disabled:cursor-not-allowed"
-          onClick={() => {setFile();setMaze([]);setCreatedSolution("");setPositionG([]); setPositionS([]); resetFile()}}
-          disabled={!file}
-        >
-          Clear File
-        </button>}
-        </div>
-      {file && positionS.length && positionG.length && maze && <GetMaze maze={maze} />}
-      {file && positionS.length && positionG.length && <div className="w-full flex justify-center my-5">
-      {<button
-        className="px-3 py-1 bg-lime-400 rounded-[3px] hover:bg-lime-300  cursor-pointer disabled:cursor-not-allowed"
-          onClick={() => {
-            getMazeSolution(maze, positionG, positionS, setCreatedSolution); setSolution(true)
+      <div
+        className={`w-full flex ${
+          file && positionG.length && positionS.length
+            ? "pt-[80px] pb-2 justify-start"
+            : "h-screen justify-center"
+        }  items-center pb-10 z-0`}
+      >
+        <input
+          className="file"
+          onChange={(e) => {
+            setFile(e);
           }}
-          disabled={!file}
-        >
-          Solve
-        </button>}
-      </div>}
-      {file && solution && <p className="text-white font-medium w-full h-[30px]">Solution : {createdSolution}</p>}
+          type="file"
+        />
+        {file && (!positionS.length || !positionG.length) && (
+          <p className="mx-[10px] font-medium text-white">
+            Please Enter valid maze
+          </p>
+        )}
+        {file && (
+          <button
+            className="px-3 py-1 bg-[#FDD36A] rounded-[3px] hover:bg-lime-300  cursor-pointer disabled:cursor-not-allowed"
+            onClick={() => {
+              setFile();
+              setMaze([]);
+              setCreatedSolution("");
+              setPositionG([]);
+              setPositionS([]);
+              resetFile();
+            }}
+            disabled={!file}
+          >
+            Clear File
+          </button>
+        )}
+      </div>
+      {file && positionS.length && positionG.length && maze && (
+        <GetMaze maze={maze} />
+      )}
+      {file && positionS.length && positionG.length && (
+        <div className="w-full flex justify-center my-5">
+          {
+            <button
+              className="px-3 py-1 bg-lime-400 rounded-[3px] hover:bg-lime-300  cursor-pointer disabled:cursor-not-allowed"
+              onClick={() => {
+                getMazeSolution(maze, positionG, positionS, setCreatedSolution);
+                setSolution(true);
+              }}
+              disabled={!file}
+            >
+              Solve
+            </button>
+          }
+        </div>
+      )}
+      {file && solution && (
+        <p className="text-white font-medium w-full h-[30px]">
+          Solution : {createdSolution}
+        </p>
+      )}
     </div>
   );
 }
